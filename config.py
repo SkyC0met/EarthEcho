2e4291d55d421c2a3cd3e73ebf9a6096817aba7b
chelsy <rosejoue06.cale@gmail.com>
2024-07-09 15:07:09 +0800

    abc

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'skycomet')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'password')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'XiaoZhan1005')
     MYSQL_HOST = os.getenv('MYSQL_HOST', '127.0.0.1')
     MYSQL_DB = os.getenv('MYSQL_DB', 'earthecho_db')
 
